c47d034f53a53397e3dbdd5bbfec47428f04656f
Matteo Carbone <mc@matteocarbone.com>
2022-02-06 22:35:00 +0100

    feat(data): add base models for agenda #2 #3

--- src/models/index.d.ts
+++ src/models/index.d.ts
@@ -1,19 +1,84 @@
 import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplify/datastore";
 
+export enum SessionType {
+  PARTNER = "PARTNER",
+  WORKSHOP = "WORKSHOP",
+  SMALL_SESSION = "SMALL_SESSION",
+  KEYNOTE = "KEYNOTE"
+}
 
 
 
+type UserFavoriteSessionMetaData = {
+  readOnlyFields: 'createdAt' | 'updatedAt';
+}
+
+type VenueMetaData = {
+  readOnlyFields: 'createdAt' | 'updatedAt';
+}
+
+type SpeakerMetaData = {
+  readOnlyFields: 'createdAt' | 'updatedAt';
+}
 
 type SessionMetaData = {
   readOnlyFields: 'createdAt' | 'updatedAt';
+}
+
+type SessionSpeakerMetaData = {
+  readOnlyFields: 'createdAt' | 'updatedAt';
+}
+
+export declare class UserFavoriteSession {
+  readonly id: string;
+  readonly sessionId: string;
+  readonly createdAt?: string;
+  readonly updatedAt?: string;
+  constructor(init: ModelInit<UserFavoriteSession, UserFavoriteSessionMetaData>);
+  static copyOf(source: UserFavoriteSession, mutator: (draft: MutableModel<UserFavoriteSession, UserFavoriteSessionMetaData>) => MutableModel<UserFavoriteSession, UserFavoriteSessionMetaData> | void): UserFavoriteSession;
+}
+
+export declare class Venue {
+  readonly id: string;
+  readonly name: string;
+  readonly createdAt?: string;
+  readonly updatedAt?: string;
+  constructor(init: ModelInit<Venue, VenueMetaData>);
+  static copyOf(source: Venue, mutator: (draft: MutableModel<Venue, VenueMetaData>) => MutableModel<Venue, VenueMetaData> | void): Venue;
+}
+
+export declare class Speaker {
+  readonly id: string;
+  readonly name: string;
+  readonly sessions?: (SessionSpeaker | null)[];
+  readonly createdAt?: string;
+  readonly updatedAt?: string;
+  constructor(init: ModelInit<Speaker, SpeakerMetaData>);
+  static copyOf(source: Speaker, mutator: (draft: MutableModel<Speaker, SpeakerMetaData>) => MutableModel<Speaker, SpeakerMetaData> | void): Speaker;
 }
 
 export declare class Session {
   readonly id: string;
   readonly name: string;
   readonly description?: string;
+  readonly type: SessionType | keyof typeof SessionType;
+  readonly startsAt: string;
+  readonly endsAt: string;
+  readonly Speakers?: (SessionSpeaker | null)[];
+  readonly Venue: Venue;
   readonly createdAt?: string;
   readonly updatedAt?: string;
+  readonly sessionVenueId: string;
   constructor(init: ModelInit<Session, SessionMetaData>);
   static copyOf(source: Session, mutator: (draft: MutableModel<Session, SessionMetaData>) => MutableModel<Session, SessionMetaData> | void): Session;
+}
+
+export declare class SessionSpeaker {
+  readonly id: string;
+  readonly speaker: Speaker;
+  readonly session: Session;
+  readonly createdAt?: string;
+  readonly updatedAt?: string;
+  constructor(init: ModelInit<SessionSpeaker, SessionSpeakerMetaData>);
+  static copyOf(source: SessionSpeaker, mutator: (draft: MutableModel<SessionSpeaker, SessionSpeakerMetaData>) => MutableModel<SessionSpeaker, SessionSpeakerMetaData> | void): SessionSpeaker;
 }
--- src/models/index.js
+++ src/models/index.js
@@ -2,10 +2,20 @@
 import { initSchema } from '@aws-amplify/datastore';
 import { schema } from './schema';
 
+const SessionType = {
+  "PARTNER": "PARTNER",
+  "WORKSHOP": "WORKSHOP",
+  "SMALL_SESSION": "SMALL_SESSION",
+  "KEYNOTE": "KEYNOTE"
+};
 
-
-const { Session } = initSchema(schema);
+const { UserFavoriteSession, Venue, Speaker, Session, SessionSpeaker } = initSchema(schema);
 
 export {
-  Session
+  UserFavoriteSession,
+  Venue,
+  Speaker,
+  Session,
+  SessionSpeaker,
+  SessionType
 };
