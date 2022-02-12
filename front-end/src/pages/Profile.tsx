a059f78abc36fa9e49d2c074186de3d50c3cdbe1
Matteo Carbone <mc@matteocarbone.com>
2022-02-12 20:36:52 +0100

    feat(profile): minimal form error handling

--- front-end/src/pages/Profile.tsx
+++ front-end/src/pages/Profile.tsx
@@ -28,31 +28,31 @@ import {
   usersFallbackImageURL
 } from '../utils/data';
 import { ESNCountries, ESNSections } from '../utils/ESNSections';
+import { UserProfile } from 'models/userProfile';
 
 const ProfilePage: React.FC = () => {
   const [showMessage] = useIonToast();
   const [showLoading, dismissLoading] = useIonLoading();
 
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [ESNCountry, setESNCountry] = useState('');
-  const [ESNSection, setESNSection] = useState('');
+  const [userProfile, setUserProfile] = useState<UserProfile>();
+  const [errors, setErrors] = useState(new Set<string>());
 
   const [avatar, setAvatar] = useState('');
   const [avatarTempImageFile, setAvatarTempImageFile] = useState<File>();
   const fileInput = createRef<HTMLInputElement>();
 
+  const handleFieldChange = (fieldName: string, value: any): void => {
+    (userProfile as any)[fieldName] = value;
+    setUserProfile(new UserProfile(userProfile));
+  };
+  const fieldHasErrors = (fieldName: string): boolean => errors.has(fieldName);
+
   useEffect(() => {
     const loadData = async () => {
-      const profile = await getUserProfile();
-      if (profile) {
-        setFirstName(profile.firstName);
-        setLastName(profile.lastName);
-        setESNCountry(profile.ESNCountry || '');
-        setESNSection(profile.ESNSection || '');
-      }
+      const userProfile = await getUserProfile();
+      setUserProfile(userProfile);
 
-      const avatar = await getUserAvatarURL(profile);
+      const avatar = await getUserAvatarURL(userProfile);
       setAvatar(avatar);
     };
     loadData();
@@ -80,8 +80,13 @@ const ProfilePage: React.FC = () => {
 
     await showLoading();
     try {
+      const errors = new Set(userProfile?.validate());
+      setErrors(errors);
+      if (errors.size !== 0) throw new Error('Invalid fields');
+
       if (avatarTempImageFile) await updateUserAvatar(avatarTempImageFile);
-      await saveUserProfile({ firstName, lastName, ESNCountry, ESNSection });
+
+      await saveUserProfile(userProfile);
       await showMessage({ ...toastMessageDefaults, message: 'Profile saved.', color: 'success' });
     } catch (err) {
       await showMessage({
@@ -106,59 +111,82 @@ const ProfilePage: React.FC = () => {
         )}
       </IonHeader>
       <IonContent>
-        <IonList style={{ maxWidth: 450, margin: '0 auto' }}>
-          <p>
-            <IonAvatar
-              style={{ margin: '0 auto', width: 100, height: 100, cursor: 'pointer' }}
-              onClick={() => fileInput.current?.click()}
-            >
-              {avatar ? (
-                <IonImg src={avatar} onIonError={(e: any) => (e.target.src = usersFallbackImageURL)} />
-              ) : (
-                <IonSkeletonText animated></IonSkeletonText>
-              )}
-            </IonAvatar>
-            <input type="file" accept="image/*" onChange={uploadNewAvatar} ref={fileInput as any} hidden={true} />
-          </p>
-          <form onSubmit={handleSubmit}>
-            <IonItem color="white">
-              <IonLabel position="floating">First name</IonLabel>
-              <IonInput required value={firstName} onIonChange={e => setFirstName(e.detail.value || '')}></IonInput>
-            </IonItem>
-            <IonItem color="white">
-              <IonLabel position="floating">Last name</IonLabel>
-              <IonInput required value={lastName} onIonChange={e => setLastName(e.detail.value || '')}></IonInput>
-            </IonItem>
-            <IonItem color="white">
-              <IonLabel position="floating">ESN Country</IonLabel>
-              <IonSelect interface="popover" value={ESNCountry} onIonChange={e => setESNCountry(e.detail.value)}>
-                {ESNCountries.map(country => (
-                  <IonSelectOption key={country} value={country}>
-                    {country}
-                  </IonSelectOption>
-                ))}
-              </IonSelect>
-            </IonItem>
-            <IonItem color="white">
-              <IonLabel position="floating">ESN Section</IonLabel>
-              <IonSelect
-                interface="popover"
-                value={ESNSection}
-                disabled={!ESNCountry}
-                onIonChange={e => setESNSection(e.detail.value)}
+        {userProfile ? (
+          <IonList style={{ maxWidth: 450, margin: '0 auto' }}>
+            <p>
+              <IonAvatar
+                style={{ margin: '0 auto', width: 100, height: 100, cursor: 'pointer' }}
+                onClick={() => fileInput.current?.click()}
               >
-                {ESNSections.filter(s => s.country === ESNCountry).map(s => (
-                  <IonSelectOption key={s.section} value={s.section}>
-                    {s.section}
-                  </IonSelectOption>
-                ))}
-              </IonSelect>
-            </IonItem>
-            <IonButton type="submit" expand="block" style={{ marginTop: 20 }}>
-              Save changes
-            </IonButton>
-          </form>
-        </IonList>
+                {avatar ? (
+                  <IonImg src={avatar} onIonError={(e: any) => (e.target.src = usersFallbackImageURL)} />
+                ) : (
+                  <IonSkeletonText animated></IonSkeletonText>
+                )}
+              </IonAvatar>
+              <input type="file" accept="image/*" onChange={uploadNewAvatar} ref={fileInput as any} hidden={true} />
+            </p>
+            <form onSubmit={handleSubmit}>
+              <IonItem color="white">
+                <IonLabel position="floating">First name</IonLabel>
+                <IonInput
+                  required
+                  value={userProfile.firstName}
+                  onIonChange={e => handleFieldChange('firstName', e.detail.value)}
+                  className={fieldHasErrors('firstName') ? 'fieldHasError' : ''}
+                ></IonInput>
+              </IonItem>
+              <IonItem color="white">
+                <IonLabel position="floating">Last name</IonLabel>
+                <IonInput
+                  required
+                  value={userProfile.lastName}
+                  onIonChange={e => handleFieldChange('lastName', e.detail.value)}
+                  className={fieldHasErrors('lastName') ? 'fieldHasError' : ''}
+                ></IonInput>
+              </IonItem>
+              <IonItem color="white">
+                <IonLabel position="floating">ESN Country</IonLabel>
+                <IonSelect
+                  interface="popover"
+                  value={userProfile.ESNCountry}
+                  onIonChange={e => {
+                    userProfile.ESNSection = '';
+                    handleFieldChange('ESNCountry', e.detail.value);
+                  }}
+                  className={fieldHasErrors('ESNCountry') ? 'fieldHasError' : ''}
+                >
+                  {ESNCountries.map(country => (
+                    <IonSelectOption key={country} value={country}>
+                      {country}
+                    </IonSelectOption>
+                  ))}
+                </IonSelect>
+              </IonItem>
+              <IonItem color="white">
+                <IonLabel position="floating">ESN Section</IonLabel>
+                <IonSelect
+                  interface="popover"
+                  value={userProfile.ESNSection}
+                  disabled={!userProfile.ESNCountry}
+                  onIonChange={e => handleFieldChange('ESNSection', e.detail.value)}
+                  className={fieldHasErrors('ESNSection') ? 'fieldHasError' : ''}
+                >
+                  {ESNSections.filter(s => s.country === userProfile.ESNCountry).map(s => (
+                    <IonSelectOption key={s.section} value={s.section}>
+                      {s.section}
+                    </IonSelectOption>
+                  ))}
+                </IonSelect>
+              </IonItem>
+              <IonButton type="submit" expand="block" style={{ marginTop: 20 }}>
+                Save changes
+              </IonButton>
+            </form>
+          </IonList>
+        ) : (
+          ''
+        )}
       </IonContent>
     </IonPage>
   );
