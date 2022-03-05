7bf2ef61a802c5cbed1c413f977b27173923d22f
Ricardo Bento <bento.ricardo.f@gmail.com>
2022-03-05 17:23:51 +0000

    added submit info to organisation functionality - WIP

--- front-end/src/pages/Organization.tsx
+++ front-end/src/pages/Organization.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
-import { IonContent, IonPage, useIonToast, useIonViewWillEnter } from '@ionic/react';
+import { IonContent, IonPage, useIonToast, useIonViewWillEnter, useIonLoading, IonButton, IonAlert } from '@ionic/react';
+import { getUserProfile } from '../utils/data';
+import Auth from '@aws-amplify/auth';
 
 import { Organization } from 'models/organization';
 import { toastMessageDefaults } from '../utils';
@@ -10,6 +12,8 @@ import EntityHeader from '../components/EntityHeader';
 
 const OrganizationPage: React.FC = () => {
   const [showMessage] = useIonToast();
+  const [showLoading, dismissLoading] = useIonLoading();
+  const [showAlert, setShowAlert] = useState(false);
 
   const [organization, setOrganization] = useState<Organization>();
 
@@ -26,6 +30,28 @@ const OrganizationPage: React.FC = () => {
     }
   };
 
+  const submitContactInfo = async (event: any): Promise<void> => {
+    // add loading
+    // add toast messages
+    event.preventDefault();
+
+    if (!organization) return;
+
+    const userProfile = await getUserProfile();
+
+    if (!userProfile) return;
+
+    if (!userProfile.contactEmail) {
+      const user = await Auth.currentAuthenticatedUser();
+      userProfile.contactEmail = user.attributes.email;
+    }
+
+    const { firstName, lastName, contactEmail, contactPhone, hasUploadedCV } = userProfile;
+
+    // at confirmation an email will be sent to contactEmail
+
+  }
+
   return (
     <IonPage>
       <EntityHeader
@@ -37,6 +63,67 @@ const OrganizationPage: React.FC = () => {
         <div className="cardContainer">
           <OrganizationCard organization={organization}></OrganizationCard>
         </div>
+        <IonButton onClick={() => setShowAlert(true)} expand="block">I'd like to get in contact</IonButton>
+        <IonAlert
+          isOpen={showAlert}
+          onDidDismiss={() => setShowAlert(false)}
+          cssClass='my-custom-class'
+          header={'Choose info to send'}
+          inputs={[
+            {
+              name: 'Name',
+              type: 'checkbox',
+              label: 'Name',
+              checked: true,
+              disabled: true
+            },
+            {
+              name: 'Email',
+              type: 'checkbox',
+              label: 'Email',
+              checked: true,
+              disabled: true
+            },
+            {
+              name: 'Phone Nr.',
+              type: 'checkbox',
+              label: 'Phone Nr.',
+              handler: () => {
+                console.log('Phone Nr. selected');
+              }
+            },
+            {
+              name: 'CV',
+              type: 'checkbox',
+              label: 'CV',
+              handler: () => {
+                console.log('CV selected');
+              }
+            }
+          ]}
+          buttons={[
+            {
+              text: 'Cancel',
+              handler: () => {
+                // return true to close the alert (is it needed?)
+                return true;
+              },
+              role: 'cancel',
+              cssClass: 'secondary'
+            },
+            {
+              text: 'Submit',
+              handler: (inputData) => {
+                // change to the appropriate handler
+                alert('submit button pressed');
+                // just to check if the checkboxes are passed this way
+                console.log(inputData);
+                // return true to close the alert (is it needed?)
+                return true;
+              }
+            }
+          ]}
+        />
       </IonContent>
     </IonPage>
   );
