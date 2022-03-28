11917ab2cfcca29c0eff09aa7927dc89e5e1cf9d
Matteo Carbone <mc@matteocarbone.com>
2022-03-28 18:47:42 +0200

    feat(organizations): add pagination

--- front-end/src/pages/Organizations.tsx
+++ front-end/src/pages/Organizations.tsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useHistory } from 'react-router';
 import {
   IonCol,
   IonContent,
   IonGrid,
   IonHeader,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
   IonItem,
   IonLabel,
   IonList,
@@ -21,8 +23,12 @@ import { Organization } from 'models/organization';
 import OrganizationCard from '../components/OrganizationCard';
 import Searchbar from '../components/Searchbar';
 
+const PAGINATION_NUM_MAX_ELEMENTS = 24;
+
 const OrganizationsPage: React.FC = () => {
   const history = useHistory();
+
+  const searchbar = useRef(null);
 
   const [organizations, setOrganizations] = useState<Array<Organization>>();
   const [filteredOrganizations, setFilteredOrganizations] = useState<Array<Organization>>();
@@ -34,19 +40,32 @@ const OrganizationsPage: React.FC = () => {
   const loadData = async (): Promise<void> => {
     const organizations = await getOrganizations();
     setOrganizations(organizations);
-    setFilteredOrganizations(organizations);
+    setFilteredOrganizations(organizations.slice(0, PAGINATION_NUM_MAX_ELEMENTS));
   };
 
-  const filterOrganizations = (search = ''): void => {
-    let filteredOrganizations: Organization[];
+  const filterOrganizations = (search = '', scrollToNextPage?: any): void => {
+    const startPaginationAfterId = filteredOrganizations?.length
+      ? filteredOrganizations[filteredOrganizations.length - 1].organizationId
+      : null;
 
-    filteredOrganizations = (organizations || []).filter(x =>
+    let filteredList: Organization[];
+
+    filteredList = (organizations || []).filter(x =>
       search
         .split(' ')
         .every(searchTerm => [x.name, x.description].filter(x => x).some(f => f.toLowerCase().includes(searchTerm)))
     );
 
-    setFilteredOrganizations(filteredOrganizations);
+    let indexOfLastOfPreviousPage = 0;
+
+    if (scrollToNextPage && filteredList.length > PAGINATION_NUM_MAX_ELEMENTS)
+      indexOfLastOfPreviousPage = filteredList.findIndex(x => x.organizationId === startPaginationAfterId) || 0;
+
+    filteredList = filteredList.slice(0, indexOfLastOfPreviousPage + PAGINATION_NUM_MAX_ELEMENTS);
+
+    if (scrollToNextPage) setTimeout(() => scrollToNextPage.complete(), 100);
+
+    setFilteredOrganizations(filteredList);
   };
 
   return (
@@ -63,7 +82,12 @@ const OrganizationsPage: React.FC = () => {
       <IonContent>
         {organizations ? (
           <IonList>
-            <Searchbar placeholder="Filter by name..." filterFn={filterOrganizations} refreshFn={loadData}></Searchbar>
+            <Searchbar
+              placeholder="Filter by name..."
+              filterFn={filterOrganizations}
+              refreshFn={loadData}
+              ref={searchbar}
+            ></Searchbar>
             <IonGrid className="ion-no-padding">
               <IonRow className="ion-justify-content-center">
                 {!filteredOrganizations ? (
@@ -87,6 +111,15 @@ const OrganizationsPage: React.FC = () => {
                     </IonCol>
                   ))
                 )}
+              </IonRow>
+              <IonRow>
+                <IonCol>
+                  <IonInfiniteScroll
+                    onIonInfinite={event => filterOrganizations((searchbar as any)?.current?.value, event?.target)}
+                  >
+                    <IonInfiniteScrollContent></IonInfiniteScrollContent>
+                  </IonInfiniteScroll>
+                </IonCol>
               </IonRow>
             </IonGrid>
           </IonList>
