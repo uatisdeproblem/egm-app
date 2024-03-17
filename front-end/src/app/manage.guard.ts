21d12e7fe7b4617e150ac9bc9cb44ecb256b042c
rbento1096 <rbento1096@gmail.com>
2024-03-17 21:37:18 +0000

    fix: refactored app guard and added admin override for admin guard

--- front-end/src/app/manage.guard.ts
+++ front-end/src/app/manage.guard.ts
@@ -5,9 +5,5 @@ import { AppService } from './app.service';
 
 export const manageGuard: CanActivateFn = async (): Promise<boolean> => {
   const _app = inject(AppService);
-  return (
-    _app.user.permissions.isAdmin ||
-    _app.user.permissions.canManageContents ||
-    _app.user.permissions.canManageRegistrations
-  );
+  return _app.userCanManageSomething()
 };
--- front-end/src/app/spot.guard.ts
+++ front-end/src/app/spot.guard.ts
@@ -5,5 +5,6 @@ import { AppService } from './app.service';
 
 export const spotGuard: CanActivateFn = async (): Promise<boolean> => {
   const _app = inject(AppService);
-  return _app.user.spot?.paymentConfirmedAt ? true : false;
+  if (_app.userCanManageSomething()) return true
+  else return _app.user.spot?.paymentConfirmedAt ? true : false;
 };
